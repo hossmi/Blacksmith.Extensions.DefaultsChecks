e3f58b8f4aa20906d35edf4eec06eef42dd120ac
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-08-12 11:57:07 +0200

    add some usefull classes

---
+++ blaxpro.Tools/Models/DescribedEnumeration.cs
@@ -0,0 +1,12 @@
+﻿namespace blaxpro.Tools.Models
+{
+    public abstract class DescribedEnumeration : Enumeration
+    {
+        protected DescribedEnumeration(int id, string name, string description) : base(id, name)
+        {
+            this.Description = description;
+        }
+
+        public string Description { get; }
+    }
+}
---
+++ blaxpro.Tools/Strings/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Everis.ToolBox;
+
+namespace Everis.ToolBox.Extensions.Strings
+{
+    public static class StringExtensions
+    {
+        public static bool isFilled(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text) == false;
+        }
+
+        public static bool isEmpty(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+    }
+}
