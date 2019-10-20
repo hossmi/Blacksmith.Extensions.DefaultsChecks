ae719af3830e084df7e551a9fa476090056aef76
hossmi <hossmi@gmail.com>
2019-10-20 09:01:18 +0200

    remove IQuery feature

--- Blacksmith.Tools/Extensions/Enumerables/EnumerableExtensions.cs
+++ Blacksmith.Tools/Extensions/Enumerables/EnumerableExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Blacksmith.Validations;
+﻿using Blacksmith.Tools.Extensions.Strings;
+using Blacksmith.Validations;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Blacksmith.Extensions.Queryables
 {
@@ -27,6 +29,77 @@ namespace Blacksmith.Extensions.Queryables
             Asserts.Assert.isNotNull(result);
 
             return result;
+        }
+
+        public static IEnumerable<T> whereIf<T>(this IEnumerable<T> items, bool condition, Func<T, bool> predicate)
+        {
+            if (condition)
+                return items.Where(predicate);
+            else
+                return items;
+        }
+
+        public static IEnumerable<T> whereIfStringIsFilled<T>(this IEnumerable<T> items, string text, Func<T, bool> predicate)
+        {
+            return whereIf<T>(items, text.isFilled(), predicate);
+        }
+
+        public static T single<T, TException>(this IEnumerable<T> items
+            , Func<T, bool> predicate
+            , Func<InvalidOperationException, TException> wrapException)
+            where TException : Exception
+        {
+            try
+            {
+                return items.Single<T>(predicate);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw wrapException(ex);
+            }
+        }
+
+        public static T single<T, TException>(this IEnumerable<T> items
+            , Func<InvalidOperationException, TException> wrapException)
+            where TException : Exception
+        {
+            try
+            {
+                return items.Single<T>();
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw wrapException(ex);
+            }
+        }
+
+        public static T singleOrDefault<T, TException>(this IEnumerable<T> items
+            , Func<T, bool> predicate
+            , Func<InvalidOperationException, TException> wrapException)
+            where TException : Exception
+        {
+            try
+            {
+                return items.SingleOrDefault<T>(predicate);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw wrapException(ex);
+            }
+        }
+
+        public static T singleOrDefault<T, TException>(this IEnumerable<T> items
+            , Func<InvalidOperationException, TException> wrapException)
+            where TException : Exception
+        {
+            try
+            {
+                return items.SingleOrDefault<T>();
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw wrapException(ex);
+            }
         }
     }
 }
--- Blacksmith.Tools/Extensions/Queryables/QueryableExtensions.cs
+++ Blacksmith.Tools/Extensions/Queryables/QueryableExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Blacksmith.Tools.Extensions.Strings;
@@ -16,80 +15,9 @@ namespace Blacksmith.Tools.Extensions.Queryables
                 return query;
         }
 
-        public static IEnumerable<T> whereIf<T>(this IEnumerable<T> items, bool condition, Func<T, bool> predicate)
-        {
-            if (condition)
-                return items.Where(predicate);
-            else
-                return items;
-        }
-
         public static IQueryable<T> whereIfStringIsFilled<T>(this IQueryable<T> query, string text, Expression<Func<T, bool>> predicate)
         {
             return whereIf<T>(query, text.isFilled(), predicate);
-        }
-
-        public static IEnumerable<T> whereIfStringIsFilled<T>(this IEnumerable<T> items, string text, Func<T, bool> predicate)
-        {
-            return whereIf<T>(items, text.isFilled(), predicate);
-        }
-
-        public static T single<T, TException>(this IEnumerable<T> items
-            , Func<T, bool> predicate
-            , Func<InvalidOperationException, TException> wrapException)
-            where TException: Exception
-        {
-            try
-            {
-                return items.Single<T>(predicate);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw wrapException(ex);
-            }
-        }
-
-        public static T single<T, TException>(this IEnumerable<T> items
-            , Func<InvalidOperationException, TException> wrapException)
-            where TException : Exception
-        {
-            try
-            {
-                return items.Single<T>();
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw wrapException(ex);
-            }
-        }
-
-        public static T singleOrDefault<T, TException>(this IEnumerable<T> items
-            , Func<T, bool> predicate
-            , Func<InvalidOperationException, TException> wrapException)
-            where TException : Exception
-        {
-            try
-            {
-                return items.SingleOrDefault<T>(predicate);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw wrapException(ex);
-            }
-        }
-
-        public static T singleOrDefault<T, TException>(this IEnumerable<T> items
-            , Func<InvalidOperationException, TException> wrapException)
-            where TException : Exception
-        {
-            try
-            {
-                return items.SingleOrDefault<T>();
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw wrapException(ex);
-            }
         }
     }
 }
