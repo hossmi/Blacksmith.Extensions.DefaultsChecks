7a372289ca193d488a1417f22b06872a52d5b64f
hossmi <hossmi@gmail.com>
2020-01-02 11:57:49 +0100

    implement tests

---
+++ Blacksmith.Extensions.DefaultsChecks.Tests/TestClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Blacksmith.Extensions.DefaultsChecks.Tests
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+            this.SomeInt = 55;
+            this.SomeText = "default";
+        }
+        public int SomeInt { get; set; }
+        public string SomeText { get; set; }
+    }
+}
